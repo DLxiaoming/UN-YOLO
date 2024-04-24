1eec1e12e138144bb051254c8da0503effc0af6d
DLxiaoming <124358757+DLxiaoming@users.noreply.github.com>
2024-04-24 21:59:11 +0800

    Update val.py

--- val.py
+++ val.py
@@ -3,8 +3,8 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO(r'ultralytics-20240222\runs\exp\weights\best.pt') 
-    model.val(data=r'ultralytics-20240222\ultralytics-main\POT-DET\data.yaml',
+    model = YOLO(r'UN-YOLO\runs\exp\weights\best.pt') 
+    model.val(data=r'UN-YOLO\POT-DET\data.yaml',
               split='val',
               imgsz=640,
               batch=16,
